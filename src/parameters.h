63df0a96ce3b56922ea5fb59be5dcc0867a8a689
Romain Feron <romainferon91@gmail.com>
2018-07-13 08:49:16 +0200

    Added gff parameters

--- src/parameters.h
+++ src/parameters.h
@@ -10,15 +10,23 @@ struct Parameters{
     float fixed_range;
     uint window_size;
     uint output_resolution;
+
     uint male_pool;
     bool output_coverage;
     bool output_snps_pos;
+    bool output_genes = false;
+
     std::string input_file_path;
+    std::string gff_file_path;
     std::string output_file_path;
+
     std::ifstream input_file;
+    std::ifstream gff_file;
+
     std::ofstream snps_output_file;
     std::ofstream snps_pos_output_file;
     std::ofstream fst_threshold_output_file;
     std::ofstream fst_window_output_file;
     std::ofstream coverage_output_file;
+    std::ofstream genes_output_file;
 };
